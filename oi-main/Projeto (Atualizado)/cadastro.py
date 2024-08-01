3906f265f5f7b7531ce1e9b4025795f8002b95bc
Mateusdorvis <mateusdorvis@gmail.com>
2024-08-01 11:15:34 +0000

    ok

--- oi-main/Projeto (Atualizado)/cadastro.py
+++ oi-main/Projeto (Atualizado)/cadastro.py
@@ -1,2 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+
+class Cadastro:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title('Cadastro')
+        
