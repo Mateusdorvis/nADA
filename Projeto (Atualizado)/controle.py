224d9f879237b296f524b92a4ea4a52998db8660
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-03 22:42:00 +0000

    ok

--- Projeto (Atualizado)/controle.py
+++ Projeto (Atualizado)/controle.py
@@ -11,10 +11,12 @@ class Controle:
     def __init__(self, root):
         self.root = root
         self.cadastro = Cadastro(self.root)
-        self.button_enviar = Buttoncustomizado(self.cadastro.box_frame, text='Enviar cadastro', width=20, command=self.cadastro.enviarCadastro)
-        self.button_enviar.grid(row=13, column=0, pady=5, padx=10)
+    
 
     def salvar_usuario(self):
+        self.button_enviar = Buttoncustomizado(self.cadastro.box_frame, text='Enviar cadastro', width=20, command=self.cadastro.enviarCadastro)
+        
+        self.button_enviar.grid(row=13, column=0, pady=5, padx=10)
         nome_cadastrado = self.cadastro.nome_get()
         data_cadastrada = self.cadastro.formata_data()
         senha_cadastrada = self.cadastro.senha_get()
