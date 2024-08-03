0ce4f255365a478b9eb000da660f82e35855fca5
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-03 22:38:41 +0000

    ok

--- Projeto (Atualizado)/controle.py
+++ Projeto (Atualizado)/controle.py
@@ -1,27 +1,33 @@
 import tkinter as tk
+
 from elementos_tkinter import Labelcustomizada, LabelcustomizadaTitulo, Buttoncustomizado, Mensagens
+from login import Login
 from cadastro import Cadastro
 from Salvadados import SalvarUsuario
+from datetime import datetime
+
 
 class Controle:
     def __init__(self, root):
         self.root = root
         self.cadastro = Cadastro(self.root)
-        self.button_salvar = Buttoncustomizado(self.root, text='Salvar Usuário', command=self.salvar_usuario)
-        self.button_salvar.pack()
+        self.button_enviar = Buttoncustomizado(self.cadastro.box_frame, text='Enviar cadastro', width=20, command=self.cadastro.enviarCadastro)
+        self.button_enviar.grid(row=13, column=0, pady=5, padx=10)
 
     def salvar_usuario(self):
         nome_cadastrado = self.cadastro.nome_get()
         data_cadastrada = self.cadastro.formata_data()
         senha_cadastrada = self.cadastro.senha_get()
+        data = datetime.strptime(data_cadastrada, '%Y-5m-%d')
 
-        if data_cadastrada:
-            save_users = SalvarUsuario(nome_cadastrado, data_cadastrada, senha_cadastrada)
-            print('Usuário salvo com sucesso!')
-        else:
-            Mensagens.msgAtencao('Data inválida. O usuário não foi salvo.')
 
-if __name__ == '__main__':
-    root = tk.Tk()
+        save_users = SalvarUsuario(nome_cadastrado, data, senha_cadastrada)
+        print(save_users)
+
+
+if __name__=='__main__':
+    root= tk.Tk()
     app = Controle(root)
     root.mainloop()
+        
+
