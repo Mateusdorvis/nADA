d80919679135ebc78e3934a76367e2553225c2e1
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-03 21:31:06 +0000

    ku

--- Projeto (Atualizado)/controle.py
+++ Projeto (Atualizado)/controle.py
@@ -13,9 +13,11 @@ class Controle:
 
     def salvar_usuario(self):
         nome_cadastrado = self.cadastro.nome_get()
+        data_cadastrada = self.cadastro.formata_data()
         senha_cadastrada = self.cadastro.senha_get()
 
-        save_users = SalvarUsuario(nome_cadastrado,  senha_cadastrada)
+        save_users = SalvarUsuario(nome_cadastrado, data_cadastrada, senha_cadastrada)
+        
 
 if __name__=='__main__':
     root= tk.Tk()
