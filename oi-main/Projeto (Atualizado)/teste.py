f9f1f2e85ed9e2c8385d465576d035cb907236b0
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-03 20:10:28 +0000

    ol

--- oi-main/Projeto (Atualizado)/teste.py
+++ oi-main/Projeto (Atualizado)/teste.py
@@ -1,8 +1,17 @@
-#string
+import tkinter as tk
 
-string = 'Qual seu nome ? Mateus'
-ler = len('Qual seu nome ?')
-print(ler)
-remova = string[ler:]
-print(remova)
+class Cadastro:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Título do Cadastro')  # Define o título padrão para Cadastro
+
+class Login(Cadastro):
+    def __init__(self, root):
+        super().__init__(root)
+        self.root.title('Login de usuário')  # Sobrescreve o título na classe Login
+
+if __name__ == "__main__":
+    root = tk.Tk()  # Cria a instância principal da janela
+    app = Login(root)  # Passa a instância principal para a classe Login
+    root.mainloop()  # Inicia o loop principal da interface gráfica
 
