7060ab8f75c7855827215873712294ba556a6099
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-03 22:43:47 +0000

    ok

--- Projeto (Atualizado)/Salvadados.py
+++ Projeto (Atualizado)/Salvadados.py
@@ -39,8 +39,8 @@ class SalvarUsuario:
         try:
             self.cursor.execute( """
             INSERT INTO dados_usuarios (nome_usuarios, datas_de_nascimentos, senhas_usuarios)
-            VALUES (%s, %s, %s,);
-            """, (self.nome_usuario, self.data_usuario, self.senha_usuario))
+            VALUES (%s, %s, %s);
+            """, (self.nome_usuario, self.data_usuario, self.senha_usuario,))
                 
             if self.nome_usuario.endswith('a'):
                 print(f' A Usuária {self.nome_usuario} foi inserida na tabela com sucesso !')
