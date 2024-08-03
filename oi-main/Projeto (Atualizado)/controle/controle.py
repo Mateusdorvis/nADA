9e42bdbe47c131cc69935296ff6c9353136d8c77
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-03 20:58:52 +0000

    ok

--- oi-main/Projeto (Atualizado)/controle/controle.py
+++ oi-main/Projeto (Atualizado)/controle/controle.py
@@ -1,9 +1,14 @@
-import tkinter as tk
-from tkinter import ttk
+import sys
+import os
+
+# Adiciona o diretório pai do script ao sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from view.elementos_tkinter import Labelcustomizada, LabelcustomizadaTitulo, Buttoncustomizado, Mensagens
 from view.login import Login
 from view.cadastro import Cadastro
 from modelo.Salvadados import SalvarUsuario
+
 
 class Controle:
     def __init__(self, root):
