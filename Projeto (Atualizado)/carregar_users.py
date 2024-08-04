ca79bfc0e6cae5519f3628f2df953dfa9be828d6
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-04 02:55:35 +0000

    vcccv

--- Projeto (Atualizado)/carregar_users.py
+++ Projeto (Atualizado)/carregar_users.py
@@ -2,10 +2,7 @@ import mysql.connector
 from datetime import datetime
 
 class CarregarUsuario:
-    def __init__(self, nome_usuario : str, data_usuario : datetime , senha_usuario : str):
-        self.nome_usuario = nome_usuario
-        self.data_usuario = data_usuario
-        self.senha_usuario = senha_usuario
+    def __init__(self,  : str, data_usuario : datetime , senha_usuario : str):
         self.conexao = mysql.connector.connect(
             host="localhost",       
             user="app",             
@@ -13,11 +10,22 @@ class CarregarUsuario:
             database="app_jogos"    
         )
         self.cursor = self.conexao.cursor()
-        self.criar_tabela_dados_usuarios()
-        self.inserir_usuario_nas_tabelas()
         self.show_users()
         self.conexao.commit()
         self.conexao.close()
+    
+    def show_users(self):
+        self.cursor.execute("SELECT * FROM dados_usuarios;")
+        self.mostrar_usuarios = self.cursor.fetchall()
+        self.dicio_pessoa = {}
+        contar = 0
+        for usuario in self.mostrar_usuarios:
+            self.dicio_pessoa[f'usuário {contar + 1} '] = {'ID': usuario[0],
+            'Nome' :  usuario[1], 
+            'Data de nascimento ': usuario[2], 
+            'Senha'  : usuario[3]
+            }
+            print(self.dicio_pessoa)
         
 
   
