9292013a34eef47af4835dc9ee4506871530134e
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-04 19:38:46 +0000

    dfd

--- Projeto (Atualizado)/sistema_login.py
+++ Projeto (Atualizado)/sistema_login.py
@@ -8,7 +8,6 @@ class BaseCadastro:
     def __init__(self):
         self.root = tk.Tk()
         self.frame_caixa = Framecustomizado(self.root,width=400, height=400)
-        self.frame_caixa.grid_propagate(False) 
         self.frame_caixa.grid(row=0, column=0)
         self.root.grid_columnconfigure(0, weight=1)
         self.root.grid_rowconfigure(0, weight=1)
