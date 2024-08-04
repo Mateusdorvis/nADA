9b07567e90fd7bff7892918aee2ddc249bc12ffc
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-04 22:43:35 +0000

    sdsd

--- Projeto (Atualizado)/controle.py
+++ Projeto (Atualizado)/controle.py
@@ -70,40 +70,35 @@ class Controle:
         self.data = self.registro.data_get()
 
         def verificar_campo_vazio():
-            def verificar__cor():
-                nome_status = self.registro.nome_dicas
-                senha_status = self.registro.senha_dicas
-                data_status = self.registro.data_dicas
+            nome_status = self.registro.nome_dicas
+            senha_status = self.registro.senha_dicas
+            data_status = self.registro.data_dicas
 
-                if nome_status.cget('fg')=='red' and data_status.cget('fg')=='red' and senha_status.cget('fg')=='red':
-                    Mensagens.msgAtencao('Os três campos não seguem os requisitos ! Preencha por favor!')
-
-                elif nome_status.cget('fg')!='red' and data_status.cget('fg')=='red' and senha_status.cget('fg')=='red':
-                    Mensagens.msgAtencao('Os campos data  e senha   não seguem os requisitos ! Preencha por favor!')
-                
-                elif nome_status.cget('fg')=='red' and data_status.cget('fg')!='red' and senha_status.cget('fg')=='red':
-                    Mensagens.msgAtencao('Os campos nome  e data   não seguem os requisitos ! Preencha por favor!')
-                
-                elif nome_status.cget('fg')=='red' and data_status.cget('fg')=='red' and senha_status.cget('fg')!='red':
-                    Mensagens.msgAtencao('Os campos nome  e senha   não seguem os requisitos ! Preencha por favor!')
-                
-                elif senha_status.cget('fg')=='red':
-                    Mensagens.msgAtencao('O campo senha não segue requisitos ! Preencha por favor!')
-                
-                elif nome_status.cget('fg')=='red':
-                    Mensagens.msgAtencao('O campo nome não segue requisitos ! Preencha por favor!')
-                
-                elif data_status.cget('fg')=='red':
-                    Mensagens.msgAtencao('O campo data não segue requisitos ! Preencha por favor!')
-                
-                else:
-                     Mensagens.msgInfo('Os valores digitados seguem todos os requisitos !')
-                verificar__cor()
-
-            
             
             if self.nome == '' and self.senha == '' and self.data == '':
                 Mensagens.msgAtencao('Os três campos estão vazios, preencha por favor!')
+            
+
+            elif nome_status.cget('fg')=='red' and data_status.cget('fg')=='red' and senha_status.cget('fg')=='red':
+                Mensagens.msgAtencao('Os três campos não seguem os requisitos ! Preencha por favor!')
+
+            elif nome_status.cget('fg')!='red' and data_status.cget('fg')=='red' and senha_status.cget('fg')=='red':
+                Mensagens.msgAtencao('Os campos data  e senha   não seguem os requisitos ! Preencha por favor!')
+            
+            elif nome_status.cget('fg')=='red' and data_status.cget('fg')!='red' and senha_status.cget('fg')=='red':
+                Mensagens.msgAtencao('Os campos nome  e data   não seguem os requisitos ! Preencha por favor!')
+            
+            elif nome_status.cget('fg')=='red' and data_status.cget('fg')=='red' and senha_status.cget('fg')!='red':
+                Mensagens.msgAtencao('Os campos nome  e senha   não seguem os requisitos ! Preencha por favor!')
+            
+            elif senha_status.cget('fg')=='red':
+                Mensagens.msgAtencao('O campo senha não segue requisitos ! Preencha por favor!')
+            
+            elif nome_status.cget('fg')=='red':
+                Mensagens.msgAtencao('O campo nome não segue requisitos ! Preencha por favor!')
+            
+            elif data_status.cget('fg')=='red':
+                Mensagens.msgAtencao('O campo data não segue requisitos ! Preencha por favor!')
             
             elif self.nome != '' and self.senha == '' and self.data == '':
                 Mensagens.msgAtencao('Os  campos data e senha estão vazios, preencha por favor!')
@@ -122,6 +117,7 @@ class Controle:
                 Mensagens.msgAtencao('O campo data está vazio, preencha por favor!')
             else:
                 Mensagens.msgInfo('Seu cadastro foi realizado com sucesso !')
+            
             verificar_campo_vazio()
 
      
