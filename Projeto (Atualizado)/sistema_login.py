a8465349c2d4462b15e6f7b3e67094fac039c92e
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-04 18:51:35 +0000

    fdfdf

--- Projeto (Atualizado)/sistema_login.py
+++ Projeto (Atualizado)/sistema_login.py
@@ -7,7 +7,7 @@ from elementos_tkinter import Labelcustomizada, LabelcustomizadaTitulo, Buttoncu
 class BaseCadastro:
     def __init__(self):
         self.root = tk.Tk()
-        self.frame_caixa = Framecustomizado(self.root, width=500, height=500)
+        self.frame_caixa = Framecustomizado(self.root, width=300, height=300)
         self.frame_caixa.grid(row=0, column=0)
         self.root.grid_columnconfigure(0, weight=1)
         self.root.grid_rowconfigure(0, weight=1)
@@ -16,16 +16,16 @@ class BaseCadastro:
         self.root.mainloop()
     
     def entrada_nome(self):
-        self.nome_titulo = LabelcustomizadaTitulo(self.root, text='CAMPO NOME.')
+        self.nome_titulo = LabelcustomizadaTitulo(self.frame_caixa, text='CAMPO NOME.')
         self.nome_titulo.grid(row=0, column=0, sticky=tk.NSEW)
 
-        self.nome_label = Labelcustomizada(self.root, text='Digite seu nome')
+        self.nome_label = Labelcustomizada(self.frame_caixa, text='Digite seu nome')
         self.nome_label.grid(row=1, column=0, sticky=tk.NSEW)
 
-        self.nome_entrada = Textcustomizado(self.root)
+        self.nome_entrada = Textcustomizado(self.frame_caixa)
         self.nome_entrada.grid(row=1, column=1, sticky=tk.NSEW)
 
-        self.nome_dicas = Labelcustomizada(self.root)
+        self.nome_dicas = Labelcustomizada(self.frame_caixa)
         self.nome_dicas.grid(row=2, column=0, sticky=tk.NSEW)
 
 
