e6065e11c60d7f12f1435b84e825058af8f85864
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-04 18:27:58 +0000

    dfdfd

--- Projeto (Atualizado)/sistema_login.py
+++ Projeto (Atualizado)/sistema_login.py
@@ -4,4 +4,8 @@ from tkinter import Widget, ttk
 from tkinter import messagebox
 from elementos_tkinter import Labelcustomizada, LabelcustomizadaTitulo, Buttoncustomizado, Mensagens
 
-class 
+class BaseCadastro:
+    def __init__(self, root):
+        self.root = root
+        
+        
