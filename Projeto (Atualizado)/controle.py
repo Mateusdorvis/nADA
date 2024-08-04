8bb3d3af91eada31faa6572e16548811e72537db
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-04 00:42:48 +0000

    ok

--- Projeto (Atualizado)/controle.py
+++ Projeto (Atualizado)/controle.py
@@ -12,6 +12,10 @@ class Controle:
         self.cadastro = Cadastro(self.root)
         self.button_enviar = Buttoncustomizado(self.cadastro.box_frame, text='Enviar cadastro', command=self.enviarCadastro)
         self.button_enviar.grid(row=15, column=0, sticky=tk.NSEW)
+
+        self.cadastro.nome_text_entry.bind('<KeyRelease>', self.EventoCampoNome)
+
+        self.cadastro.senha_text_entry.bind('<KeyRelease>', self.EventoCampoSenha)
     
     def enviarCadastro(self):
         if self.cadastro.nome_get()=='' and self.cadastro.senha_get()=='' and self.cadastro.data_get()=='':
@@ -52,17 +56,17 @@ class Controle:
             if self.ler_campo_senha<=1:
                 self.cadastro.nome_status.config(text=f'Senha insuficente, pois tem {self.ler_campo_senha()} caractere !')
 
-    def EventoCampoS(self):
-        if self.ler_campo_senha()>=5 and self.ler_campo_senha()<=9:
-            self.cadastro.nome_status.config(text=f'Sua senha atingiu número de caracteres mínimo, pois tem {self.ler_campo_senha()} !')
+    def EventoCampoNome(self):
+        if self.ler_campo_nome()>=5 and self.ler_campo_nome()<=9:
+            self.cadastro.nome_status.config(text=f'Seu nome atingiu número de caracteres mínimo, pois tem {self.ler_campo_nome()} !')
 
         elif self.ler_campo_senha()==10:
-            self.cadastro.nome_status.config(text=f'Sua senha atingiu número de caracteres máximo, pois tem {self.ler_campo_senha()} !')
+            self.cadastro.nome_status.config(text=f'Seu nome atingiu número de caracteres máximo, pois tem {self.ler_campo_nome()} !')
         
         else:
-            self.cadastro.nome_status.config(text=f'Escreva mais, porque sua senha  tem  apenas {self.ler_campo_senha() } caracteres !')
-            if self.ler_campo_senha<=1:
-                self.cadastro.nome_status.config(text=f'Senha insuficente, pois tem {self.ler_campo_senha()} caractere !')
+            self.cadastro.nome_status.config(text=f'Escreva mais, porque seu nome  tem  apenas {self.ler_campo_nome() } caracteres !')
+            if self.ler_campo_nome<=1:
+                self.cadastro.nome_status.config(text=f'Nome de usuário insuficente, pois tem {self.ler_campo_nome()} caractere !')
 
 
 
