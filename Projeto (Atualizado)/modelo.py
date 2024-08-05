e0382be6fb4715560e177d0fa1f53847764063fa
Mateusdorvis004 <115734016+Mateusdorvis@users.noreply.github.com>
2024-08-05 02:09:45 +0000

    sasa

--- Projeto (Atualizado)/modelo.py
+++ Projeto (Atualizado)/modelo.py
@@ -3,23 +3,30 @@ from datetime import datetime
 from elementos_tkinter import Mensagens
 
 class SalvarUsuario:
-    def __init__(self, nome_usuario : str, data_usuario : datetime , senha_usuario : str):
+    def __init__(self, nome_usuario: str, data_usuario: datetime, senha_usuario: str):
         self.nome_usuario = nome_usuario
         self.data_usuario = data_usuario
         self.senha_usuario = senha_usuario
-        self.conexao = mysql.connector.connect(
-            host="localhost",       
-            user="app",             
-            password="oi",         
-            database="app_jogos"    
-        )
-        self.cursor = self.conexao.cursor()
-        self.criar_tabela_dados_usuarios()
-        self.inserir_usuario_nas_tabelas()
-        self.show_users()
-        self.conexao.commit()
-        self.conexao.close()
         
+        try:
+            self.conexao = mysql.connector.connect(
+                host="localhost",
+                user="app",
+                password="oi",
+                database="app_jogos"
+            )
+            self.cursor = self.conexao.cursor()
+            self.criar_tabela_dados_usuarios()
+            self.inserir_usuario_nas_tabelas()
+            self.show_users()
+        except mysql.connector.Error as err:
+            print(f"Erro de conexão: {err}")
+        finally:
+            if self.cursor:
+                self.cursor.close()
+            if self.conexao:
+                self.conexao.commit()
+                self.conexao.close()
 
     def criar_tabela_dados_usuarios(self):
         try:
@@ -31,68 +38,78 @@ class SalvarUsuario:
                 senhas_usuarios VARCHAR(255) NOT NULL
             );
             """)
-            print(f' Tabela criada com sucesso !')
-        except ValueError:
-            print(f'Erro ao criar tabela !')
+            print('Tabela criada com sucesso!')
+        except mysql.connector.Error as err:
+            print(f'Erro ao criar tabela: {err}')
 
     def inserir_usuario_nas_tabelas(self):
-       
         try:
-            self.cursor.execute( """
+            self.cursor.execute("""
             INSERT INTO dados_usuarios (nome_usuarios, datas_de_nascimentos, senhas_usuarios)
             VALUES (%s, %s, %s);
             """, (self.nome_usuario, self.data_usuario, self.senha_usuario,))
                 
             if self.nome_usuario.endswith('a'):
-                print(f' A Usuária {self.nome_usuario} foi inserida na tabela com sucesso !')
+                print(f'A Usuária {self.nome_usuario} foi inserida na tabela com sucesso!')
             else:
-                print(f' O Usuário {self.nome_usuario} foi inserido na tabela com sucesso !')
-
-
-        except ValueError:
-            print(f'Erro ao inserir o usuário {self.nome_usuario} !')
+                print(f'O Usuário {self.nome_usuario} foi inserido na tabela com sucesso!')
+        except mysql.connector.Error as err:
+            print(f'Erro ao inserir o usuário {self.nome_usuario}: {err}')
 
     def show_users(self):
-        self.cursor.execute("SELECT * FROM dados_usuarios;")
-        self.mostrar_usuarios = self.cursor.fetchall()
-        self.dicio_pessoa = {}
-        contar = 0
-        for usuario in self.mostrar_usuarios:
-            self.dicio_pessoa[f'usuário {contar + 1} '] = {'ID': usuario[0],
-            'Nome' :  usuario[1], 
-            'Data de nascimento ': usuario[2], 
-            'Senha'  : usuario[3]
-            }
-            print(self.dicio_pessoa)
-
+        try:
+            self.cursor.execute("SELECT * FROM dados_usuarios;")
+            self.mostrar_usuarios = self.cursor.fetchall()
+            self.dicio_pessoa = {}
+            contar = 0
+            for usuario in self.mostrar_usuarios:
+                self.dicio_pessoa[f'usuário {contar + 1} '] = {
+                    'ID': usuario[0],
+                    'Nome': usuario[1],
+                    'Data de nascimento': usuario[2],
+                    'Senha': usuario[3]
+                }
+                print(self.dicio_pessoa)
+        except mysql.connector.Error as err:
+            print(f'Erro ao mostrar usuários: {err}')
 
 class CarregarUsuario:
-    def __init__(self, nome_user  : str, senha_user : str):
+    def __init__(self, nome_user: str, senha_user: str):
         self.nome_user = nome_user
-        self.senha_user  = senha_user
-        self.conexao = mysql.connector.connect(
-            host="localhost",       
-            user="app",             
-            password="oi",         
-            database="app_jogos"    
-        )
-        self.cursor = self.conexao.cursor()
-        self.show_users()
-        self.conexao.commit()
-        self.conexao.close()
-    
+        self.senha_user = senha_user
+        
+        try:
+            self.conexao = mysql.connector.connect(
+                host="localhost",
+                user="app",
+                password="oi",
+                database="app_jogos"
+            )
+            self.cursor = self.conexao.cursor()
+            self.show_users()
+        except mysql.connector.Error as err:
+            print(f"Erro de conexão: {err}")
+        finally:
+            if self.cursor:
+                self.cursor.close()
+            if self.conexao:
+                self.conexao.close()
+
     def show_users(self):
-        self.cursor.execute("SELECT * FROM dados_usuarios;")
-        self.mostrar_usuarios = self.cursor.fetchall()
-        self.dicio_pessoa = {}
-        contar = 0
-        for usuario in self.mostrar_usuarios:
-            self.dicio_pessoa[f'usuário {contar + 1} '] = {'ID': usuario[0],
-            'Nome' :  usuario[1], 
-            'Data de nascimento ': usuario[2], 
-            'Senha'  : usuario[3]
-            }
-            if usuario[1]==self.nome_user and usuario[3]==self.senha_user:
-                Mensagens.msgInfo(f'{self.nome_user} Encontrado')
-            else:
-                Mensagens.msgAtencao(f'Não foi encontrado {self.nome_user} !')
+        try:
+            self.cursor.execute("SELECT * FROM dados_usuarios;")
+            self.mostrar_usuarios = self.cursor.fetchall()
+            self.dicio_pessoa = {}
+            for usuario in self.mostrar_usuarios:
+                self.dicio_pessoa[f'usuário {usuario[0]}'] = {
+                    'ID': usuario[0],
+                    'Nome': usuario[1],
+                    'Data de nascimento': usuario[2],
+                    'Senha': usuario[3]
+                }
+                if usuario[1] == self.nome_user and usuario[3] == self.senha_user:
+                    Mensagens.msgInfo(f'{self.nome_user} encontrado')
+                    return  # Usuario encontrado, sair da função
+            Mensagens.msgAtencao(f'Não foi encontrado {self.nome_user}!')
+        except mysql.connector.Error as err:
+            print(f'Erro ao mostrar usuários: {err}')
